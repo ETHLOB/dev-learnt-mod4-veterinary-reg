63dc53858ce4ed6fcce62f7daf29e3ef7629a55e
ETHLOB <anftorresme@unal.edu.co>
2025-09-24 00:55:21 -0500

    <update> feat: better definitions

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,7 @@
-<script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img
-        src="https://upload.wikimedia.org/wikipedia/commons/f/f1/Vitejs-logo.svg"
-        class="logo"
-        alt="Vite logo"
-      />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img
-        src="https://upload.wikimedia.org/wikipedia/commons/9/95/Vue.js_Logo_2.svg"
-        class="logo vue"
-        alt="Vue logo"
-      />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <h1 class="text-6xl font-mono text-center text-amber-800">
+    Hello World from Tailwind CSS
+  </h1>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
